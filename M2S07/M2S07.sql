9566ab1cc3abd89c54894f2b2be483e48303f8a2
Leonardo Arena <leonardoarena7@gmail.com>
2023-07-09 18:01:40 -0300

    M2S07 Ex3

--- M2S07/M2S07.sql
+++ M2S07/M2S07.sql
@@ -46,3 +46,22 @@ CREATE TABLE Biblioteca (
     FOREIGN KEY (jogo_id) REFERENCES Jogo(id),
     PRIMARY KEY (usuario_id, jogo_id)
 );
+
+-- Ex. 3
+INSERT INTO Genero (nome)
+VALUES ('Simulação'), ('Ação'), ('FPS'), ('Estratégia'), ('Outros');
+
+INSERT INTO Plataforma (nome)
+VALUES ('PS1'), ('PS2'), ('PS3'), ('PS4'), ('PS5'), ('Xbox Series S'), ('Xbox Series X'), ('PC');
+
+INSERT INTO Jogo (nome, dt_lancamento, foto, video, genero_id)
+VALUES ('The Sims 4 Base', '2014-09-02', 'caminho_da_foto', 'caminho_do_video', 1);
+
+INSERT INTO PlataformaJogo (plataforma_id, jogo_id)
+VALUES (4, 1), (5, 1), (8, 1);
+
+INSERT INTO Usuario (nome, login, senha, email, dt_nascimento, cpf, rg)
+VALUES ('Derpson da Silva', 'derpinho', 'derpinho91', 'derpinho91@hotmail.com', '12312312312', '4123123', '1991-01-01');
+
+INSERT INTO Biblioteca (usuario_id, jogo_id)
+VALUES (1, 1);
