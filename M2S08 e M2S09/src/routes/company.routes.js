71260a51e79cc6ee0acd420b25159fa1f36d7793
Leonardo Arena <leonardoarena7@gmail.com>
2023-07-12 14:48:47 -0300

    M2S08 e M2S09 Ex8

---
+++ M2S08 e M2S09/src/routes/company.routes.js
@@ -0,0 +1,20 @@
+const {
+  createOneCompany,
+  listCompanies,
+  listOneCompany,
+} = require("../controllers/company.controller");
+
+const { Router } = require("express");
+
+class CompanyRouter {
+  routesFromCompany() {
+    const companyRoutes = Router();
+    companyRoutes.post("/createOneCompany", createOneCompany);
+    companyRoutes.get("/listCompanies", listCompanies);
+    companyRoutes.get("/listOneCompany", listOneCompany);
+
+    return companyRoutes;
+  }
+}
+
+module.exports = new CompanyRouter();
