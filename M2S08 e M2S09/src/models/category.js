ee9337f9fd9814c3f53abf7b8cd35454340071e2
Leonardo Arena <leonardoarena7@gmail.com>
2023-07-12 14:11:24 -0300

    M2S08 e M2S09 Ex7

--- M2S08 e M2S09/src/models/category.js
+++ M2S08 e M2S09/src/models/category.js
@@ -2,11 +2,11 @@ const { connection } = require("../database/connection");
 const { STRING, DATE } = require("sequelize");
 
 const Category = connection.define(
-  "Category",
+  "category",
   {
     name: STRING,
-    created_at: DATE,
-    updated_at: DATE,
+    createdAt: DATE,
+    updatedAt: DATE,
   },
   { underscored: true, paranoid: true }
 );
